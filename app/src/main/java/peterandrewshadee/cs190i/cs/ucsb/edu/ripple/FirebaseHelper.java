5a8adb931a9804a1dfdb05d3a451f1ad1dc545e1
Shadee Barzin <shadeebarzin@gmail.com>
2017-06-11 21:05:45 -0700

    changed listeners in db. update db listeners on station click

--- app/src/main/java/peterandrewshadee/cs190i/cs/ucsb/edu/ripple/FirebaseHelper.java
+++ app/src/main/java/peterandrewshadee/cs190i/cs/ucsb/edu/ripple/FirebaseHelper.java
@@ -7,6 +7,7 @@ import android.media.session.MediaController;
 import android.media.session.MediaSessionManager;
 import android.media.session.PlaybackState;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,7 +17,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import kaaes.spotify.webapi.android.models.CurrentlyPlaying;
 import kaaes.spotify.webapi.android.models.UserPrivate;
@@ -117,8 +120,11 @@ class FirebaseHelper {
                     ));
 
                     //TODO: need to update DB when listener joins
-                    List<String> listenerList = new ArrayList<String>();
-                    listenerList.add("");
+//                    List<String> listenerList = new ArrayList<>();
+//                    listenerList.add("index0");
+//                    listenerList.add("index1");
+                    Map<String, String> listenerList = new HashMap<>();
+                    listenerList.put("no", "false");
                     Broadcast bc = new Broadcast(broadcasterId);
                     bc.setUserName(MainActivity.myUserName);
                     bc.setSongId(currentlyPlaying.item.id);
